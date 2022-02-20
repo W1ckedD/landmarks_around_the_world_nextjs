c39a91a0e5240b047751d6472f1813c5a59642c4
w1ckedd <alirezachoobineh@ymail.com>
2022-02-20 16:27:31 +0330

    added custom server and initialized sequelize

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Location } from '../../server';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const locations = await Location.findAll({});
+  res.status(200).json({ locations });
 }
